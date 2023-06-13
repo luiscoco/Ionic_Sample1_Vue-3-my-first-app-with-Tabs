9a3b7f943d6b8c3f228df895010c0b79a6b10e50
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2023-06-13 16:27:32 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'my-first-ionic-vue3-with-tabs-app',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
